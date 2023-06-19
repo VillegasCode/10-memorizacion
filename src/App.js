5561ab958a101d81135c0dfe44875dd95e180be5
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-19 14:46:21 -0500

    Tareas component imported

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { Gestion } from './components/Gestion';
+// import { Gestion } from './components/Gestion';
+import { Tareas } from './components/Tareas';
 
 function App() {
   return (
@@ -8,7 +9,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-          <Gestion />
+          {/* Ejecicio con el hook useMemo */}
+          <Tareas />
+          
+          {/* Ejecicio con el método React.memo */}
+          {/* <Gestion /> */}
+
       </header>
     </div>
   );
