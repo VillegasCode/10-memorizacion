3cc865c440d2cfc6ac9e7d88eab4fc020e76bb8f
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-19 13:59:57 -0500

    Getting data from a url through conseguirEmpleados = async method

--- src/components/Empleados.js
+++ src/components/Empleados.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const Empleados = () => {
-  return (
-    <div>Empleados</div>
+export const Empleados = 
+({pagina = 1}) => {
+  
+    const [empleados, setEmpleados] = useState([]);
+
+    useEffect(() => {
+        conseguirEmpleados(pagina);
+    }, []);
+
+    
+    const conseguirEmpleados = async(page) => {
+        const url = "https://reqres.in/api/users?page=" + page;
+        const peticion = await fetch(url);
+        const {data: empleados} = await peticion.json();
+
+        setEmpleados(empleados);
+
+        console.log("Se ejecutó la petición Ajax")
+    }
+
+    console.log("Se ha vuelto a renderizar empleados!!");
+  
+    return (
+    <div>
+        <p>Mostrando la página: {pagina}</p>
+        <ul className='empleados'>
+            {empleados.length >= 1 &&
+                empleados.map(empleado => {
+                    return <li key={empleado.id}>{empleado.first_name + " " + empleado.last_name}</li>
+                })}
+
+        </ul>
+    </div>
   )
 }
