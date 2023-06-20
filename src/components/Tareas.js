90f5986065bf33ae887101e2cf19c5ce9df75ebe
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-19 21:24:31 -0500

    Contadores method with useMemo to avoid problems of performance

--- src/components/Tareas.js
+++ src/components/Tareas.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 export const Tareas = () => {
 
   const [tareas, setTareas] = useState([]);
+  const [contador, setContador] = useState(5000);
 
   const guardarTareas = e => {
     e.preventDefault();
@@ -18,6 +19,29 @@ export const Tareas = () => {
 
   }
 
+  const borrarTarea = id => {
+    //Filtrar las tareas para borrar la que no quiero
+    let nuevas_tareas = tareas.filter( (tarea, indice) => indice !== id);
+
+    //Set State, guardar el nuevo listado de tareas en el estado
+    setTareas(nuevas_tareas);
+  }
+
+
+  const sumarAlContador = e => {
+    setContador(contador + 1);
+  }
+
+  const contadoresPasados = (acumulacion) => {
+    for(let i = 0; i <= acumulacion; i++) {
+      console.log("Ejecutando acumulación de contadores del pasado...");
+    }
+
+    return `Contador manual de tareas acumuladas -> ${acumulacion}`;
+  }
+
+const memoContadores = useMemo(() => contadoresPasados(contador), [contador]);
+
   return (
     <div className='tareas-container'>
         <h1>MIS TAREAS</h1>
@@ -27,12 +51,22 @@ export const Tareas = () => {
             <input type='submit' value="Guardar"/>
         </form>
 
+        {/* <h3>Contador manual de tareas: {contador}</h3> */}
+        <h3>{memoContadores}</h3>
+        <button onClick={sumarAlContador}>Sumar</button>
+
         <h3>Lista de Tareas</h3>
         {/* Mostrando tareas usando el useState*/}
         <ul>
           {
             tareas.map((tarea, indice) => {
-              return <li key={indice}>{tareas[indice]}</li>
+              return (
+                <li key={indice}>
+                    {tarea}
+                    &nbsp;
+                    <button onClick={() => borrarTarea(indice)}>X</button>
+                </li>
+                )
             })
           }
         </ul>
